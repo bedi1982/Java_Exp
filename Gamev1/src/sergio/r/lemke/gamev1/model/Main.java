affbd26abe513d2c24fe96065780d558fa536449
Sergio Rafael Lemke <bedi.com@gmail.com>
2015-05-04 22:46:22 -0300

    Testing keypress methods.

--- Gamev1/src/sergio/r/lemke/gamev1/model/Main.java
+++ Gamev1/src/sergio/r/lemke/gamev1/model/Main.java
@@ -3,8 +3,10 @@ package sergio.r.lemke.gamev1.model;
 import sergio.r.lemke.gamev1.play.Play;
 
 public class Main {
-	public static void main(String[] args) {
-		Play p = new Play();
-		p.Jogar();
-	}
+	public static void main(String[] args){
+		
+		Play pl = new Play();
+		pl.Jogar();
+}
+
 }
--- Gamev1/src/sergio/r/lemke/gamev1/play/Play.java
+++ Gamev1/src/sergio/r/lemke/gamev1/play/Play.java
@@ -1,15 +1,59 @@
 package sergio.r.lemke.gamev1.play;
-import java.util.Arrays;
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import sergio.r.lemke.gamev1.control.Metodos;
-
-public class Play{
-    	
+@SuppressWarnings("serial")
+public class Play extends Applet implements KeyListener{
+    private Rectangle rect;	
+	
 	public void Jogar() {
-		Metodos metodos = new Metodos();
+		/*Metodos metodos = new Metodos();
 		char[][] tab = new char[10][10];
 		tab = metodos.generateTable();
 		tab[0][0] = '8';
-		System.out.println(Arrays.deepToString(tab).replace("],","]\n").replace(","," "));
+		//System.out.println(Arrays.deepToString(tab).replace("],","]\n").replace(","," "));
+		*/
+		this.addKeyListener(this);
+		rect = new Rectangle(0,0,50,50);
+	}
+	
+	public void paint(Graphics g){
+		setSize(500,500);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		g2.fill(rect);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("la");
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+			rect.setLocation(rect.x +2, rect.y);
+			
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_LEFT){
+			rect.setLocation(rect.x -2, rect.y);
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_UP){
+			rect.setLocation(rect.x, rect.y - 2);
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN){
+			rect.setLocation(rect.x, rect.y + 2);
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
